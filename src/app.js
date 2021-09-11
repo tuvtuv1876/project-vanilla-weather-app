55d271fe174c49cba7fd47c59bc00fb80c43ab9c
tuvtuv1876 <87674025+tuvtuv1876@users.noreply.github.com>
2021-09-11 21:50:16 +0200

    updated day and time

--- src/app.js
+++ src/app.js
@@ -3,14 +3,19 @@ function showTemperature(response) {
   let city = document.querySelector("h3");
   let weatherDescription = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
-  let precipitation = document.querySelector("#precipitation");
   let wind = document.querySelector("#wind");
 
-  let date = new Date();
+  let date = new Date(response.data.dt * 1000);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let days = date.getDate();
-  let day = [
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -19,18 +24,19 @@ function showTemperature(response) {
     "Friday",
     "Saturday",
   ];
-  console.log(`${day[days]}`);
+  let weekday = days[day];
   let time = document.querySelector(".time");
 
   keyTemperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  precipitation.innerHTML = `Precipitation: #%`;
   wind.innerHTML = `Wind: ${response.data.main.humidity} km/h`;
+  time.innerHTML = `${weekday}, ${hours}:${minutes}`;
 }
 
 let cityElement = "Zurich";
 let apiKey = "b90f000267947bf1f6e5a78a0ca5e027";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityElement}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
 axios.get(apiUrl).then(showTemperature);
